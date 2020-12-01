0c82c783ed9d10af045ba6d57982681b5c52706f
Julius Boateng <jboateng@juliuss-mbp.lan>
2020-12-01 10:43:01 -0500

    Improved Time Complexity Solution

--- day1/day1.py
+++ day1/day1.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python3
 import sys
 
-def sum_to_2020(lines, nums):
-    if not lines:
-        return 0
+GOAL = 2020
 
-    if len(nums) > 3:
-        return 0
+def dict_pairs(lines):
+    dic = dict()
 
-    if (len(nums) == 3) and (sum(nums) == 2020):
-        return nums[0] * nums[1] * nums[2]
+    for index_1 in range(len(lines) - 1):
+        for index_2 in range(index_1, len(lines)):
+            val = GOAL - (lines[index_1] + lines[index_2])
+            
+            if val > 0:
+                dic[val] = [lines[index_1], lines[index_2]]
 
-    val = lines[0]
-    
-    result_1 = sum_to_2020(lines[1:], nums + [val])
-    result_2 = sum_to_2020(lines[1:], nums)
+    return dic
 
-    return result_1 if result_1 > 0 else result_2
+def sum_to_2020(lines, dic):
+    lines_set = set(lines)
 
-    # for index_1 in range(len(lines) - 1):
-    #     for index_2 in range(index_1, len(lines)):
-    #         if (lines[index_1] + lines[index_2]) == 2020:
-    #             return lines[index_1] * lines[index_2]
+    for val in dic:
+        if val in lines_set:
+            return val * dic[val][0] * dic[val][1]
 
 def main():
     lines = list(map(lambda num: int(num.strip()),sys.stdin))
-    print(sum_to_2020(lines,[]))
-
+    dic = dict_pairs(lines)
+    print(sum_to_2020(lines, dic))
 
 if __name__ == "__main__":
     main()
