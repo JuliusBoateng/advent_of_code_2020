2dab149e3eb931759333112d4a252f208cc1d546
Julius Boateng <jboateng@Juliuss-MBP.fios-router.home>
2021-01-01 18:28:44 -0500

    day15: Complete

---
+++ day15/day15_b.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import sys
+
+def readNums():
+    return [int(num) for num in sys.stdin.readline().split(',')]
+
+def calcNum(arrNums, nth_num):
+    dicNums = {}
+
+    for index, num in enumerate(arrNums):
+        dicNums[num] = index
+    
+    for index in range(len(dicNums), nth_num):
+        prev_index = index - 1
+        prev_val = arrNums[prev_index]
+        if prev_val not in dicNums:
+            dicNums[prev_val] = prev_index
+
+        prev_val_index = dicNums[prev_val]
+
+        curr_val = prev_index - prev_val_index
+        dicNums[prev_val] = prev_index
+
+        arrNums.append(curr_val) 
+
+    return arrNums[-1]
+
+def main():
+    arrNums = readNums()
+    nth_val = calcNum(arrNums, 30000000)
+    print(nth_val)
+
+
+
+if __name__ == "__main__":
+    main()
